9c42ecf71adabcc014bdc1d765101b3750de49e7
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2026-01-13 17:05:47 +0100

    feat: enhance ActivityManagementService with synchronization and event handling improvements

--- server/src/context/core/application/services/ActivityManagementService.ts
+++ server/src/context/core/application/services/ActivityManagementService.ts
@@ -1,49 +1,65 @@
-import { RoomRepository } from "../../domain/ports/RoomRepository";
 import {
   ExternalActivity,
   InternalActivity,
 } from "../../domain/model/Activity";
 import { Campus } from "../../../../shared/domain/Location";
-import {
-  UniboProvider,
-  NotificationService,
-  AuthService,
-} from "../../domain/ports/ServicePorts";
+import { AuthService } from "../../domain/ports/ServicePorts";
 import { EventBus } from "../../../../shared/domain/EventBus";
 import { ActivityAddedEvent } from "../../domain/events/ActivityAddedEvent";
+import { UniboProviderHTTP } from "../../infrastructure/adapters/UniboProviderHTTP";
+import { RoomRepositoryMongo } from "../../infrastructure/adapters/RoomRepositoryMongo";
 
 export class ActivityManagementService {
+  private activeFetches: Map<string, Promise<void>> = new Map();
   constructor(
-    private roomRepository: RoomRepository,
-    private uniboProvider: UniboProvider,
-    private notificationService: NotificationService,
+    private roomRepository: RoomRepositoryMongo,
+    private uniboProvider: UniboProviderHTTP,
     private authService: AuthService,
     private eventBus: EventBus,
   ) {}
 
   async syncEvent(campus: Campus, date: Date): Promise<void> {
-    const lastSync = await this.roomRepository.getLastSyncTime(campus);
+    const lastSync = await this.roomRepository.getLastSync(campus, date);
     const now = new Date();
     if (lastSync && now.getTime() - lastSync.getTime() < 3600000) {
       console.log("Sync skipped, caching.");
       return;
     }
 
-    console.log(
-      "[Sync] Fetching internal activities for campus:",
-      campus,
-      "on date:",
-      date,
-    );
-    const internalActivities: InternalActivity[] =
-      await this.uniboProvider.fetchInternalActivities(campus, date);
-    await this.roomRepository.updateInternalActivities(
-      campus,
-      date,
-      internalActivities,
-    );
-    await this.roomRepository.setLastSyncTime(campus);
-    console.log("[Sync] Completed for campus:", campus);
+    const dateKey = date.toISOString().split("T")[0];
+    const lockKey = `${campus}_${dateKey}`;
+
+    if (this.activeFetches.has(lockKey)) {
+      console.log("Sync already in progress for", lockKey);
+      return this.activeFetches.get(lockKey)!;
+    }
+
+    const syncTask = (async () => {
+      try {
+        console.log(
+          "[Sync] Fetching internal activities for campus:",
+          campus,
+          "on date:",
+          date,
+        );
+        const internalActivities: InternalActivity[] =
+          await this.uniboProvider.fetchInternalActivities(campus, date);
+        await this.roomRepository.updateInternalActivities(
+          campus,
+          date,
+          internalActivities,
+        );
+        await this.roomRepository.setLastSync(campus, date);
+        console.log("[Sync] Completed for campus:", campus);
+      } catch (error) {
+        console.error("[Sync] Error during synchronization:", error);
+        throw error;
+      } finally {
+        this.activeFetches.delete(lockKey);
+      }
+    })();
+    this.activeFetches.set(lockKey, syncTask);
+    await syncTask;
   }
 
   async createEvent(token: string, event: ExternalActivity): Promise<void> {
@@ -76,16 +92,18 @@ export class ActivityManagementService {
       endTime: event.period.end,
       description: event.description,
     });
-    await this.eventBus.publish(domainEvent);
-    //await this.notifyEvent(event); va fatto comunque?
-    console.log(
-      "[Event Created & Published] External event created and published on event bus with ID:",
-      event.id,
-    );
+    console.log("[Event Created] External event created with ID:", event.id);
+    await this.notifyEvent(domainEvent, event.id);
   }
 
-  async notifyEvent(event: ExternalActivity): Promise<void> {
-    await this.notificationService.sendEventNotification(event);
-    console.log("[Notification] Sending notification for event ID:", event.id);
+  async notifyEvent(
+    event: ActivityAddedEvent,
+    id: string | undefined,
+  ): Promise<void> {
+    await this.eventBus.publish(event);
+    console.log(
+      "[Event Published] External event published on event bus with ID:",
+      id,
+    );
   }
 }
