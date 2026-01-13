b4997cc1ce1ce1d527c8fc350fd30e105e5c61b6
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2026-01-13 17:04:22 +0100

    refactor: remove unused NotificationService interface from ServicePorts

--- server/src/context/core/domain/ports/ServicePorts.ts
+++ server/src/context/core/domain/ports/ServicePorts.ts
@@ -1,15 +1,11 @@
 import { Campus } from "../../../../shared/domain/Location";
-import { ExternalActivity, InternalActivity } from "../model/Activity";
+import { InternalActivity } from "../model/Activity";
 
 export interface UniboProvider {
   fetchInternalActivities(
     campus: Campus,
     date: Date,
   ): Promise<InternalActivity[]>;
-}
-
-export interface NotificationService {
-  sendEventNotification(event: ExternalActivity): Promise<void>;
 }
 
 export interface AuthService {
