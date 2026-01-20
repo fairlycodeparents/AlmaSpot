2eaf5be2dc52258ac41544d71338ae0231e0dbd4
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2026-01-20 18:07:09 +0100

    build(client): setup frontend (vite, tailwind, postcss)

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        almaRed: "#C81E28",
+        almaDark: "#1A1A1A",
+      },
+      borderRadius: {
+        "3xl": "1.5rem",
+      },
+    },
+  },
+  plugins: [],
+};
