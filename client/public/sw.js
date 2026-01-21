74eaa3c1e1b7b5bfc7510801f6738963e029dc50
ChiaraGiangiulli <chiara.giangiulli@studio.unibo.it>
2026-01-21 12:10:22 +0100

    feat(notification): implement service worker for push notifications and notification click handling

---
+++ client/public/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("push", function (event) {
+  const data = event.data ? event.data.json() : {};
+  const title = data.title || "AlmaSpot";
+  const options = {
+    body: data.body || "Nuovo aggiornamento!",
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener("notificationclick", function (event) {
+  event.notification.close();
+  event.waitUntil(clients.openWindow("/"));
+});
