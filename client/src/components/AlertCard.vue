810400c0405f25eb5c6109384921ffbe285b6323
ChiaraGiangiulli <chiara.giangiulli@studio.unibo.it>
2026-01-22 16:21:36 +0100

    feat(ui): add AlertCard component

---
+++ client/src/components/AlertCard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+interface AlertConfig {
+  title?: string;
+  intro?: string;
+  message: string;
+  timeSlot?: string;
+}
+
+withDefaults(defineProps<AlertConfig>(), {
+  title: "Attenzione!",
+  intro: "",
+  message: "",
+  timeSlot: "",
+});
+</script>
+
+<template>
+  <div
+    class="bg-accent text-base-text rounded-4xl p-8 shadow-lg max-w-md w-full"
+    role="alert"
+  >
+    <h2 class="text-4xl font-bold mb-3 tracking-tight">
+      {{ title }}
+    </h2>
+
+    <div class="text-lg leading-snug space-y-1">
+      <p class="opacity-90">{{ intro }}</p>
+      <p class="font-medium">
+        {{ message }} <span v-if="timeSlot">{{ timeSlot }} </span>
+      </p>
+    </div>
+  </div>
+</template>
