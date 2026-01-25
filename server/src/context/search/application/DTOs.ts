70405282ae5566e9796b4b5a4ef57b1174224160
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2026-01-25 17:22:41 +0100

    refactor: update SuggestionDTO to use PlanSlotDTO

--- server/src/context/search/application/DTOs.ts
+++ server/src/context/search/application/DTOs.ts
@@ -1,10 +1,21 @@
-import { RoomSlot } from "../domain/Entities";
+/**
+ * DTOs for RoomSlot and PlanSlot
+ */
+export interface PlanSlot {
+  readonly id: string;
+  readonly name: string;
+  readonly type: string;
+  readonly campus: string;
+  readonly address: string;
+  readonly from: Date;
+  readonly to: Date;
+}
 
 /**
  * DTOs for Suggestion response
  */
 export interface SuggestionDTO {
-  readonly plan: RoomSlot[];
+  readonly plan: PlanSlot[];
   readonly response: string;
 }
 
